8c7a9877d08afc49bde3a9248f964c14e72f0a7c
damnpoet <damnpoet@gmail.com>
2013-09-04 13:54:04 -0500

    added specifications stated on issue #11 to left menu

--- themes/sound/views/common/leftMenu.php
+++ themes/sound/views/common/leftMenu.php
@@ -8,16 +8,16 @@
         <ul class="nav">
             <?php foreach($categories as $category): ?>
             <li>
-                <a class="active" href="<?php echo $this->createUrl('/category/view', array('id'=>$category->category_id)); ?>"><?php echo $category->description->getName(); ?> (<?php echo $category->getProductsCount(); ?>)</a>
+                <a class="cat" data-cat-id="cat-<?php echo $category->category_id; ?>" href="<?php echo $this->createUrl('/category/view', array('id'=>$category->category_id)); ?>"><i id="img-cat-<?php echo $category->category_id; ?>" class="icon-angle-right"></i> <?php echo $category->description->getName(); ?> (<?php echo $category->getProductsCount(); ?>)</a>
                 <?php if($category->hasChildCategories()): ?>
-                <ul>
+                <ul id="cat-<?php echo $category->category_id; ?>" style="display: none;">
                     <?php foreach($category->childCategories as $childCategory): ?>
                     <li>
-                        <a href="<?php echo $this->createUrl('/category/view', array('id'=>$childCategory->category_id)); ?>"> - <?php echo $childCategory->description->getName(); ?> (<?php echo $childCategory->getProductsCount(); ?>)</a>
+                        <a class="cat" data-cat-id="cat-<?php echo $childCategory->category_id; ?>" href="<?php echo $this->createUrl('/category/view', array('id'=>$childCategory->category_id)); ?>"> <i id="img-cat-<?php echo $childCategory->category_id; ?>" class="icon-angle-right"></i> <?php echo $childCategory->description->getName(); ?> (<?php echo $childCategory->getProductsCount(); ?>)</a>
                         <?php if($childCategory->hasChildCategories()): ?>
-                        <ul>
+                        <ul id="cat-<?php echo $childCategory->category_id; ?>"  style="display: none;">
                             <?php foreach($childCategory->childCategories as $childCategory2): ?>
-                            <li><a href="<?php echo $this->createUrl('/category/view', array('id'=>$childCategory2->category_id)); ?>"> - <?php echo $childCategory2->description->getName(); ?> (<?php echo $childCategory2->getProductsCount(); ?>)</a></li>
+                            <li><a href="<?php echo $this->createUrl('/category/view', array('id'=>$childCategory2->category_id)); ?>"> <?php echo $childCategory2->description->getName(); ?> (<?php echo $childCategory2->getProductsCount(); ?>)</a></li>
                             <?php endforeach; ?>
                         </ul>
                         <?php endif; ?>
@@ -30,3 +30,25 @@
         </ul>
     </div><!-- end sidebar -->		
 </div>
+
+<?php
+$js = "
+    function toggleImage(imgSelector) {
+        var c = $(imgSelector).attr('class');
+        if(c == 'icon-angle-right') 
+            $(imgSelector).attr('class', 'icon-angle-down');
+        else
+            $(imgSelector).attr('class', 'icon-angle-right');
+    }
+
+    $('.cat').on('click', function(){
+        var catId = $(this).attr('data-cat-id');
+        $('#' + catId).toggle(200);
+        toggleImage('#img-' + catId);
+        
+        return false;
+    });
+";
+
+Yii::app()->clientScript->registerScript('leftMenu', $js);
+?>
