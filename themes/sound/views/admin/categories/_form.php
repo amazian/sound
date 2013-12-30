7a9b8525996baf6610ff48e6d77076ca4e55c16a
Richard González Alberto <damnpoet@gmail.com>
2013-12-30 17:43:04 -0500

    Added sort order to categories, stated on issue #33

--- themes/sound/views/admin/categories/_form.php
+++ themes/sound/views/admin/categories/_form.php
@@ -81,5 +81,11 @@ $form = $this->beginWidget('CActiveForm', array(
         <?php echo $form->dropDownList($model, 'bottomMost', array(0=>"No", 1=>"Yes"), array('class' => 'span1')); ?>
     </div>
 </div>
+<div class="control-group">
+    <?php echo $form->label($model, 'sortOrder', array('class' => 'control-label')); ?>
+    <div class="controls">
+        <?php echo $form->textField($model, 'sortOrder', array('class' => 'span1')); ?>
+    </div>
+</div>
 
 <?php $this->endWidget(); ?>
