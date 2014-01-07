6278e76d5e2d11c4b5ed56e8a13f4ec8a03f58d8
Richard González Alberto <damnpoet@gmail.com>
2014-01-07 01:38:10 -0500

    Added about page

--- themes/sound/views/site/about.php
+++ themes/sound/views/site/about.php
@@ -5,54 +5,47 @@ $this->breadcrumbs = array(
 );
 ?>
 
-<div class="span3">
-    <h2>Lightweight defaults</h2>
-    <h3>Rewritten base class</h3>
-    <p>With Bootstrap 2, we've simplified the base class: <code>.alert</code> instead of <code>.alert-message</code>. We've also reduced the minimum required markup&mdash;no <code>&lt;p&gt;</code> is required by default, just the outer <code>&lt;div&gt;</code>.</p>
-</div>
-<div class="span9">
+<div class="span12">
     <div class="page-header">
-        <h1>About us <small>What we do</small></h1>
+        <h1>公司簡介</h1>
     </div>
 
     <!-- Headings & Paragraph Copy -->
     <div class="row">
 
-
-        <div class="span9">
-            <blockquote class="pull-right">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis.</p>
-                <small>Someone famous in <cite title="">Body of work</cite></small>
-            </blockquote>
-
-        </div>
-
-        <div class="span9">
-            <h3>Why do you do what you do?</h3>
-            <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
-        </div>
-
-        <div class="span9">
-            <h3>Who are the people behind the company?</h3>
-            <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
-        </div>
-
-        <div class="span9">
-            <h3>What kind of people will I be working with/buying from?</h3>
-            <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
-        </div>
-
-        <div class="span9">
-            <h3>What does your company stand for?</h3>
-            <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
+        <div class="span12">
+            <font color="#990000">在電子材料與音響業界,佑昇公司保持著數樣特點: </font>
+            <p>　　一、本公司成立於民國62年,多年來致力於世界頂級名廠電子材料之經營, 是國內第一家專門經營高穩定性、高精密度、高信賴度、歐美名廠生產,適用於各類需於長時間運作時,亦需維持高穩定性,高精確度,舉世公認品質最優秀的
+                電子主被動零組件,所賣之材料品質保證零缺點。</p>
+            <p>　　二、以現貨供應最多世界各國名廠(約60家)電子材料,而且本公司經營之電子零件, 全部為進口材料,精挑細選優良廠牌,所供應之材料品質精良、規格齊全,庫存豐富,供貨迅速,價格合理,並設有寬敞門市部,零售服務顧客,當您生產研發高級的電子、電機產品時,佑昇將
+                是您提昇品質的得力伙伴。事業團體,研發單位,機關學校,及個人,經常需要高信賴度的電子 材料,來輔助並達成精確可靠的研發,但常常為了找尋這些高品質材料困難重重,或者需求量
+                太少,無法達到國外廠商要求之最低出貨量,而延誤商機等等這些困擾,那麼請與本公司聯絡, 也許就可解決您絕大部份的問題,數量不拘、竭誠服務、請利用傳真或來電洽詢,遠地朋友
+                請多多利用網路購買,本公司寄貨迅速,如為急件可以匯款完畢馬上將收據E-MAIL並注明姓 名及購買材料明細表,本公司保證當天出貨。</p>
+            <p> 　　三、本公司有替顧客準備冷門,規格特殊、市面上較不普遍的材料之服務精神。本公司 所經營之主動(電晶體.半導體)與被動(電阻,電容器)電子材料,在現貨供應方面,為服務顧客,準
+                備了許多型號與規格在市面上很難找到,本公司幾乎是僅有的供應商(此類材料,因為冷門難賣成本難以回收,一般商家均不會考慮賣它！)因此在電子材料市場流行一句話:找特殊材料現貨
+                時先找佑昇,如果佑昇沒有,在市面上就很難找到現貨了!因此當您要維修精密儀表及工業電子 時,請先來佑昇問看看。</p>
+            <p>　　四、佑昇的高級及精密被動元件,尤其在電阻與電容器方面,規格之齊全保證獨一無二,例 如本公司進口之各名廠精密電阻:</p>
+            <ul>
+                <li>誤差值小於±1%,溫度系數低於50ppm,於70℃時耐0.6W,阻值從1W至10MW,規格超過400種。</li>
+                <li>誤差值小於±0.1%,溫度系數低於25ppm,於70℃時耐1/4W,阻值從5W至1MW,規格超過350種。</li>
+                <li>誤差值小於±0.01%,溫度系數低於10ppm,於70℃時耐1/4W, 也備有常用規格。</li>
+                <li>誤差值小於±1%,溫度系數低於50ppm,於70℃時耐1W及2W,阻值從1W至1MW,也各有上百種規格。</li>
+                <li>5W. 10W及10W以上線繞電阻規格也相當齊全,更備有高級儀表專用±1% 3W.5W低溫度系數、無感線繞電阻 </li>
+            </ul>
+            <p>電機工業用小體積耐高壓電阻:誤差值小於±1%,溫度系數低於200PPM,耐溫155℃,於70℃時耐1/4W、1/2W及1W。 1/2 W耐壓3500VDC,
+                1W耐壓10000 VDC,阻值從1M~33MW(1/2W)及100K~68 MW(1W)各數十種常用規格。 電容器方面:備有適合各類電路及各種用途之高級電容器,電容量從0.47PF至1500000mF規格齊全,而且全部由世界名廠生產供應,材質有:
+                銀膜陶瓷電容、銀雲母電容、鉭質電容、電解電容、金屬膜電容(有KT、MKP、MKC、PP、PS等材質)共有數種規格。其中更備有不同廠牌各有數拾種規格,低損失、低溫度系數、低誤差值(±1%,±2.5%),材質高級:PP(聚丙烯膜),
+                PS(聚苯乙烯膜),等精密儀表級專用電容器,以供選擇。</p>
+            <p> 　　五、本公司是唯一除了經營高級電子材料業務外,亦致力於 Hi-END級音響及其週邊產品之研究與生產, 並且平價供應音響迷的公司,20幾年來佑昇除致力於世界知名電子材料
+                之經營外,對於音響精品材料之引進更是不遺餘力,也是國內第一家進口高級電子零件及專用於頂級音響之主、被動材料的公司,圓了無數追求 Hi-END DIY族的理想與興趣。在世界頂尖級音響器材之看得到的發燒級元件,在佑昇
+                幾乎都能找到,當您要為自己心愛的音響系統升級,無論是要裝機、改機、(包括任何主機、喇叭等）,請您務必來一趟佑昇,我們竭誠為您提供意見及技術上的支援與服務,讓您達到事半功倍的效果,音質更臻完美的境界。</p>
+            <p>　　本公司從事高級音響及套件之研發也已超過20年,深知如何做好高級音響的每一細節與訣竅,產品款款精良, 本公司自20年前即率先推出高水準音響套件,造成極大轟動與迴響,因為當時經營音響套件廠家至少有幾十家,但都是生產一些很普通等級的套件產品,如今佑昇是經營Hi-END級音響套件碩果僅存的公司,佑昇套件不但在國內
+                擁有相當多的愛用者,在世界各地也極為出名,本公司套件只要不錯銲,不冷銲,成功率百分之百。</p>
+            <p>　　本公司所生產之成品機,全機由內到外,注重每一生產過程與細節,精挑細選最高級的歐美零件,與本公司精心研發,由高純度OFC高 級軟銅製造之 Hi-END級音響專用線材,及音響專用銲錫,由銲接經驗豐富的師傅以專業化的德國製恆溫烙鐵,純手工精細銲製,機器不但具備優良性,更能將音樂美感發揮淋漓盡致,因此在佑昇,您只需用極合理的代價,即可擁有上
+                百萬級進口機也未必能達到之高級享受,本公司備有試聽室歡迎光臨,並歡迎顧客帶器材來參考比較(無論主機、 線材、喇叭等皆歡迎)。</p>
+            <p> 　　本公司精心開發一系列高級機箱,多年來嘉惠無數喜好音響DIY 人士,使用高級鋁合金材料,精密加工,處理精良,外型簡潔高雅,賞心悅目,各種用途均備:如電源專用箱、晶體前級、晶體後級、真空管前級、真空管後級、純A類及超大功率後級專用等每款機箱,並備有黑、白、香檳三種顏色面板,可供選擇,歡迎選用。</p>
+            <p>　　本公司亦專精於音響系統之維修,技術人員經驗豐富,細心負責,保證使用比原廠更優良的零件做修護,因為在本公司只有高級材料也因此使維修後的器材性能不但恢復而且提昇。</p>
+            <p><font color="#000099">本公司本著卓越商譽為您服務,請多指教。</font></p>
         </div>
 
 
