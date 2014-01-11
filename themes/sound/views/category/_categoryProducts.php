27608146d0e1704e4007765190fdbc42dbbf60c7
Richard González Alberto <damnpoet@gmail.com>
2014-01-11 15:41:46 -0500

    Show only products with preview attribute activated, issue #52

--- themes/sound/views/category/_categoryProducts.php
+++ themes/sound/views/category/_categoryProducts.php
@@ -1,6 +1,7 @@
 <table class="table table-striped">
     <tbody>
         <?php foreach ($category->activeProducts as $product): ?>
+            <?php if(!$product->preview) continue; ?>
             <tr>
                 <td><a href="<?php echo $this->createUrl('/product/view', array('id' => $product->product_id)); ?>"><img src="<?php echo $product->getImageWithSize(60, 60); ?>" id="tmp" alt=""></a></td>
                 <td><a class="product_popover" data-title="<?php echo $product->description->getName(); ?> / <?php echo $product->type; ?> / <?php echo $product->model; ?>" data-product-id="<?php echo $product->product_id; ?>" href="<?php echo $this->createUrl('/product/view', array('id' => $product->product_id)); ?>"><?php echo $product->description->getName(); ?></a></td>
