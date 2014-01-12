265ca8028ee4887bca94ace765aeed4a503f8941
Richard González Alberto <damnpoet@gmail.com>
2014-01-11 19:02:25 -0500

    Added tags box to left menu, issue #61

--- themes/sound/views/common/leftMenu.php
+++ themes/sound/views/common/leftMenu.php
@@ -5,53 +5,47 @@ Yii::app()->clientScript->registerCoreScript('cookie');
 <?php $categories = Category::model()->firstLevel()->active()->orderBySortOrder()->findAll(); ?>
 <div class="span3">
     <!-- start sidebar -->
-    <ul class="breadcrumb">
-        <li>Categories</span></li>
-    </ul>
-    <div id="column-left" class="span3">
+    <div class="categories-container">
+        <ul class="breadcrumb">
+            <li>Categories</li>
+        </ul>
+        <div id="column-left" class="span3">
+            <?php
+            /*$this->widget(
+                'CTreeView',
+                array(
+                    'url' => array('/site/ajaxFillTree'),
+                    'persist' => 'cookie'
+                )
+            );*/
+            $this->widget('ext.yii-jqTree.JQTree', array(
+                'dataUrl' => $this->createAbsoluteUrl('/site/ajaxFillTree'),
+                'dragAndDrop' => false,
+                'selectable' => false,
+                'saveState' => 'leftTree',
+                'autoOpen' => true,
+                'onCreateLi' => "js:function(node, \$li) {
+                    \$li.find('.jqtree-title').html('<a href=\"".$this->createUrl("/category")."/'+ node.id +'\">'+ node.name +'</a>');
+                }"
+                /*'htmlOptions' => array(
+                    'class' => 'treeview-red',
+                ),*/
+            ));
+            ?>
+        </div><!-- end sidebar -->
+    </div>
+    <div class="clearfix"></div>
+    <div class="tags-container">
+        <ul class="breadcrumb">
+            <li>Tags</li>
+        </ul>
         <?php
-        /*$this->widget(
-            'CTreeView',
-            array(
-                'url' => array('/site/ajaxFillTree'),
-                'persist' => 'cookie'
-            )
-        );*/
-        $this->widget('ext.yii-jqTree.JQTree', array(
-            'dataUrl' => $this->createAbsoluteUrl('/site/ajaxFillTree'),
-            'dragAndDrop' => false,
-            'selectable' => false,
-            'saveState' => 'leftTree',
-            'autoOpen' => true,
-            'onCreateLi' => "js:function(node, \$li) {
-                \$li.find('.jqtree-title').html('<a href=\"".$this->createUrl("/category")."/'+ node.id +'\">'+ node.name +'</a>');
-            }"
-            /*'htmlOptions' => array(
-                'class' => 'treeview-red',
-            ),*/
-        ));
+        $tags = ProductTag::model()->findAllBySql("SELECT * FROM product_tag GROUP BY tag_text");
+        foreach($tags as $tag):
         ?>
-    </div><!-- end sidebar -->		
+            <a href="#" class="btn btn-mini"><?php echo $tag->tag_text; ?></a>
+        <?php endforeach; ?>
+    </div>
+    <br />
+    <br />
 </div>
-
-<?php
-$js = "
-    function toggleImage(imgSelector) {
-        var c = $(imgSelector).attr('class');
-        if(c == 'icon-angle-right') 
-            $(imgSelector).attr('class', 'icon-angle-down');
-        else
-            $(imgSelector).attr('class', 'icon-angle-right');
-    }
-
-    $('.cat').on('click', function(){
-        var catId = $(this).attr('data-cat-id');
-        $('#' + catId).toggle(200);
-        toggleImage('#img-' + catId);
-        
-        return false;
-    });
-";
-
-Yii::app()->clientScript->registerScript('leftMenu', $js);
-?>
