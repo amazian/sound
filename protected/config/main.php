df93baf053ec37d03f666f4008950336089f8fcf
root <root@amazian.vpshosting24.com>
2014-03-29 11:18:29 -0400

    updated main config

--- protected/config/main.php
+++ protected/config/main.php
@@ -24,12 +24,12 @@ return array(
     'modules' => array(
         // uncomment the following to enable the Gii tool
 
-        'gii' => array(
+        /*'gii' => array(
             'class' => 'system.gii.GiiModule',
             'password' => '123',
             // If removed, Gii defaults to localhost only. Edit carefully to taste.
             'ipFilters' => array('127.0.0.1', '::1'),
-        ),
+        ),*/
         
         'admin' => array(
             'class' => 'application.modules.admin.AdminModule',
@@ -103,6 +103,6 @@ return array(
     // application-level parameters that can be accessed
     // using Yii::app()->params['paramName']
     'params' => array(
-        'imagePath' => '/var/www/sandbox/sound/image/',
+        'imagePath' => '/srv/sandbox.amazianteam.com/htdocs/sound/image/',
     ),
 );
