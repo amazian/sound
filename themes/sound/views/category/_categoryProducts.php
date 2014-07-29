4ffd2878bfe1a8f0cbe82d622c9ba359226ddef3
amazian <amazianteam@hotmail.com>
2014-07-29 08:08:41 -0400

    Fix for issue #105

--- themes/sound/views/category/_categoryProducts.php
+++ themes/sound/views/category/_categoryProducts.php
@@ -6,7 +6,12 @@
                 <td><a href="<?php echo $this->createUrl('/product/view', array('id' => $product->product_id)); ?>"><img src="<?php echo $product->getImageWithSize(60, 60); ?>" id="tmp" alt=""></a></td>
                 <td><a class="product_popover" data-title="<?php echo $product->description->getName(); ?> / <?php echo $product->type; ?> / <?php echo $product->model; ?>" data-product-id="<?php echo $product->product_id; ?>" href="<?php echo $this->createUrl('/product/view', array('id' => $product->product_id)); ?>"><?php echo $product->description->getName(); ?></a></td>
                 <td><?php echo $product->model; ?></td>
-                <td><?php echo isset($product->primarySpec) ? $product->primarySpec->value : "&nbsp;"; ?></td>
+                <td>
+                    <?php if(isset($product->primarySpec)): ?>
+                    <?php $primerySpec = $product->primarySpec; ?>
+                    <?php echo $primerySpec->value_init; ?><?php echo ($primerySpec->value_end != '') ? ' ~ ' . $primerySpec->value_end : ''; ?><?php echo (!is_null($primerySpec->unit)) ? ' ' . $primerySpec->unit->name : ''; ?>
+                    <?php endif; ?>
+                </td>
                 <td><?php echo!is_null($product->manufacturer) ? $product->manufacturer->name : '' ?></td>
                 <td><?php echo $product->getFormattedPrice(true); ?></td>
                 <td>Qty: <?php echo CHtml::textField('qty', 1, array('class'=>'span1 quantity')); ?></td>
